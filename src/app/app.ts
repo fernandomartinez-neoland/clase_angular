3a801b818affc8a83d905f9bf5f31bc3285ad555
fernandomartinez-neoland <fernando.martinez@neoland.es>
2025-12-02 13:45:20 +0100

    suma

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('clase_angular');
+  title = signal('cdqwdqwdwdr');
+
+  numA = signal(5);
+  numB = signal(10);
+  updateA($event: any) {
+    this.numA.set($event);
+  }
+  updateB($event: any) {
+    this.numB.set($event);
+  }
+  suma = signal(0);
+
+  sumatoria() {
+    this.suma.set(this.numA() + this.numB());
+  }
 }
